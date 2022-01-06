2cc795b7d4be56567e41cb75ab6b038f8908f2de
ylem76 <ylem76@thebreeze.co.kr>
2022-01-06 09:53:57 +0900

    220106 angryProfessor 설명추가

--- algorithm-study/hacker-rank/implementation/angryProfessor.js
+++ algorithm-study/hacker-rank/implementation/angryProfessor.js
@@ -1,4 +1,8 @@
 function angryProfessor(k, a) {
+    /*
+    filter 함수를 이용해 제 시간에 온 학생의 배열을 구함
+    배열의 length를 이용해 k를 비교해 yes or no 리턴
+    */
 
     const onTimeStudentNum = a.filter(item => item<=0).length;
     const isCanceled = onTimeStudentNum >= k ? 'NO' : 'YES';
