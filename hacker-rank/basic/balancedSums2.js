e338b623cc3d68d7976e433cd7621a2deff3c8f8
ylem76 <ylem76@thebreeze.co.kr>
2022-05-16 09:50:09 +0900

    220516 balancedSums3

--- hacker-rank/basic/balancedSums2.js
+++ hacker-rank/basic/balancedSums2.js
@@ -1,9 +1,16 @@
 /**
  * 대부분 이런 로직으로 풀었던데 왜 때문에 시간 초과가 뜨는지...
- * 
+ * 합 구하는 파트를 reduce 함수에서 for 반복문으로 변경 -> 통과
+ * reduce가 내 생각보다 느린가보다
  */
 function balancedSums(arr) {
-	let rightSum = arr.reduce((a,b) => a+b);
+	//let rightSum = arr.reduce((a,b) => a+b);
+	
+	let rightSum = 0;
+    for(let i=0; i<arr.length; i++) {
+        rightSum += arr[i]
+	}
+	
 	let leftSum = 0;
 	
 	for(let i=0; i<arr.length; i++) {
