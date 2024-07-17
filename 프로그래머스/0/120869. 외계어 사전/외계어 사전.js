314ac139c1563a779a6f3f9b8c9cb12dcc9e310c
song song <68649488+ylem76@users.noreply.github.com>
2024-07-17 22:03:28 +0900

    [level 0] Title: 외계어 사전, Time: 3.57 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
@@ -2,18 +2,18 @@ function solution(spell, dic) {
     console.log(spell, dic)
     var answer = 2;
     
-    
+    // 길이가 다른 문자 삭제
     const filtered = dic.filter(word => word.length === spell.length);
     
+    // 이중for문으로 각각의 글자 비교
     for(let i=0; i<filtered.length; i++) {
         const word = filtered[i].split('');
         for(j=0; j<spell.length; j++) {
             if(word.includes(spell[j])) {
-                console.log(word)
-                console.log(j===spell.length -1)
+                // 마지막 글자까지 다 맞으면 존재하는 것
                 if(j===spell.length -1) {
                     answer = 1;
-                    
+                    return answer;
                 }
             } else {
                 break;
